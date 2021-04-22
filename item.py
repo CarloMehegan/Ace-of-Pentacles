674d316c54656bb3f73594557dfe9726ff244b93
Micah Cessna <>
2021-04-22 15:12:14 +0000

    Added inventory access command

--- item.py
+++ item.py
@@ -1,10 +1,25 @@
-inv=Bag()
+from adventurelib import *
 
+inv=Bag()
+here=Room("""You are in a place""")
+here.items = Bag([
+ Item("apple"),
+])
+current_room=here
 @when("take the ITEM")
 @when("take ITEM")
 def take(item):
-  if current_room.items.take(item):
+  object =current_room.items.take(item)
+  if not object:
+    print(f"There is no {item} here.")
+  else:
     inv.add(item)
     print(f"You take the {item}.")
-  else:
-    print(f"There is no {item} here.")
+@when("inventory")
+def inventory():
+  print("Your inventory contains:")
+  if not inv:
+    print ("nothing")
+    return
+  for item in inv:
+    print (f'{item}')
