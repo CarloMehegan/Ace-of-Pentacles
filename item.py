0cbd66c46fa32bb5d48060edf71029b02de5afd3
Micah Cessna <>
2021-04-27 15:13:13 +0000

    Clarification Inventory condencation

--- item.py
+++ item.py
@@ -1,12 +1,12 @@
 from adventurelib import *
 
-inv=Bag()
+
 here=Room("""You are in a place""")
 here.items = Bag([
  Item("apple"),
 ])
 current_room=here
-
+## command to create look around commands for rooms with associated bags.
 def get_look(description):
   @when("look around")
   def look():
@@ -17,24 +17,33 @@ def get_look(description):
         print(f'{item}')
     else:
       print("Nothing")
+## Overarching inventory function
+def load_inventory():
+  inv=Bag()
 
-@when("take the ITEM")
-@when("take ITEM")
-def take(item):
-  object =current_room.items.take(item)
-  if not object:
-    print(f"There is no {item} here.")
-  else:
-    inv.add(item)
-    print(f"You take the {item}.")
+  ##place item in room bag into inventory bag
+  @when("take the ITEM")
+  @when("take ITEM")
+  def take(item):
+    object =current_room.items.take(item)
+    if not object:
+      print(f"There is no {item} here.")
+    else:
+      inv.add(item)
+      print(f"You take the {item}.")
+##list inventory bag
+  @when("inventory")
+  def inventory():
+    print("Your inventory contains:")
+    if not inv:
+      print ("nothing")
+      return
+    for item in inv:
+      print (f'{item}')
 
-@when("inventory")
-def inventory():
-  print("Your inventory contains:")
-  if not inv:
-    print ("nothing")
-    return
-  for item in inv:
-    print (f'{item}')
-
-def create_room(name,description,bag)
+def create_room(name,description,bag):
+  ##create a room contaiining a bag of items
+  name=Room(description)
+  name.items=Bag([
+    Item()
+  ])
