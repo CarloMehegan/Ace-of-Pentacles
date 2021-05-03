1e0347933fef50a6352526bc54485f0504f83897
Jon Stapleton <>
2021-05-03 17:13:20 +0000

    added some example commands

--- main.py
+++ main.py
@@ -1,30 +1,22 @@
 from adventurelib import *
 
-@when("exclaim")
-def yell():
-  print("You bellow at the top of your lungs.")
 
-@when("enter the cave", context='field')
-def go_cave():
-  set_context('cave')
-  print("You enter the cave")
+@when("look around")
+def look_woods():
+  print(f"You find yourself in a {get_context()}")
 
-@when("exit the cave", context='cave')
-def exit_cave():
-  set_context('field')
-  print("You exit the cave")
+@when("enter the CTX")
+def change_context(ctx):
+  set_context(ctx) # change context
+  print(f"You enter the {ctx}.")
 
-@when("take THING")
-def take(thing):
-  print(f"You take the {thing}.")
+@when("search", context="woods")
+def search_woods():
+  print("You find a stick")
 
-@when("look around", context="cave")
-def look_cave():
-  print("You find yourself in a dark, damp cave.")
+@when("search", context="field")
+def search_field():
+  print("You find a sheep. BAAAA.")
 
-@when("look around", context="field")
-def look_field():
-  print("You find yourself in a bright, sunny field")
-
-set_context('field') # built-in function
+set_context("woods")
 start()
