a34b151ed970812a31fdaafad33510e7605c33b5
Xander Martinez <>
2021-05-04 15:00:55 +0000

    I've completed the rest of the puzzle

--- main.py
+++ main.py
@@ -3,31 +3,7 @@ from puzzle import *
 
 set_context("puzzle")
 
-@when("exclaim")
-def yell():
-  print("You bellow at the top of your lungs.")
 
-@when("enter the cave", context='field')
-def go_cave():
-  set_context('cave')
-  print("You enter the cave")
-
-@when("exit the cave", context='cave')
-def exit_cave():
-  set_context('field')
-  print("You exit the cave")
-
-@when("take THING")
-def take(thing):
-  print(f"You take the {thing}.")
-
-@when("look around", context="cave")
-def look_cave():
-  print("You find yourself in a dark, damp cave.")
-
-@when("look around", context="field")
-def look_field():
-  print("You find yourself in a bright, field")
 
 #set_context('field') # built-in function
 start()
--- puzzle.py
+++ puzzle.py
@@ -22,7 +22,7 @@ def hints():
 
 @when("riddle", context='puzzle')
 def riddle():
-  print(" the letter states in order to leave the room you need to solve a riddle.")
+  print(" I am full of holes but I still hold water.")
 
 
 
@@ -33,6 +33,45 @@ def solve(answer):
   else:
     print("that answer is wrong")
   
-@when("riddle", context='puzzle')
-def riddle():
-  print(" the letter states in order to leave the room you need to solve a riddle.")
+@when("walk", context='puzzle')
+def walk():
+  print(" you walk to the open door it leads to an other room.")
+
+@when("keys", context='puzzle')
+def keys ():
+  print(" you see three keys one is made of gold the second silver the third tungsten..")
+
+@when("door", context='puzzle')
+def door():
+  print(" there are 3 doors for three keys the locks on the doors have numbers on them they represent melting points find the correct melting point for the three keys.")
+
+@when(" melting", context='puzzle')
+def melting():
+ print(" the first door is 1,763°f the second is 1,948°f the third is 6,192°f .")
+
+gold_door = Item("the second door")
+gold_door.locked = True
+
+silver_door = Item("the first door")
+silver_door.locked = True 
+
+tungsten_door = Item("the third door")
+tungsten_door.locked = True
+
+
+@when("try the KEY key in the DOOR door")
+def try_door(key, door):
+  if key == "gold" and door == "second":
+    print("You are successful!")
+    gold_door.locked = False
+  elif key == "silver" and door == "first":
+   silver_door.locked = False 
+   print ("you are successful!")
+  elif key == "tungsten"and door == "third":
+     print ("you are successful!")
+     tungsten_door.locked = False
+  else:
+    print(f"The {key} key doens't fit in the {door} door")
+
+  if not gold_door.locked and not silver_door.locked and not tungsten_door.locked:
+    print("All the door merge into a portal you have successfully escaped congrats")
